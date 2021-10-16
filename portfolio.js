cd3824bdbe5630eb5cc0dd52fe6800b20a1e7f4e
AlondraLDiaz <79497412+AlondraLDiaz@users.noreply.github.com>
2021-10-15 20:26:20 -0600

    portfolio
    all the stuffs

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+  // Open and close Menu//
+  function w3_open() {
+    document.getElementById("mobileMenu").style.width = "100%";
+    document.getElementById("mobileMenu").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mobileMenu").style.display = "none";
+  }
+  
